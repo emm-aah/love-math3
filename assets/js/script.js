35cc00af31f318277528bd6fe448f255553a660b
emm-aah <>
2024-01-10 16:50:23 +0000

    fix code for subtract game

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,10 +96,13 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
-  document.getElementById("operand1").textContent = operand1 > operand2 ? operand1:operand2;
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand2 : operand1;
-    document.getElementById("operator").textContent = "-";
+
+function displaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
+
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
